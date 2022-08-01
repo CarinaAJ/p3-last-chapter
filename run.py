844f00be1b38e482003de4aff44a8d8101c76361
CarinaAJ <carinastenger@yahoo.com>
2022-08-01 10:17:08 +0000

    Add order book function for user to select title

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 import time 
 
 book_list = {
-    "1.": "Title1 - Author1",
-    "2.": "Title2 - Author2",
-    "3.": "Title3 - Author3",
-    "4.": "Title4 - Author4",
-    "5.": "Title5 - Author5",
-    "6.": "Title6 - Author6",
-    "7.": "Title7 - Author7",
-    "8.": "Title8 - Author8",
+    "1": "Title1 - Author1",
+    "2": "Title2 - Author2",
+    "3": "Title3 - Author3",
+    "4": "Title4 - Author4",
+    "5": "Title5 - Author5",
+    "6": "Title6 - Author6",
+    "7": "Title7 - Author7",
+    "8": "Title8 - Author8",
 }
 
 #works, just need to make sure to add validation error 
@@ -19,6 +19,7 @@ def enter_or_exit():
     """
     while True:
         print("Hello and welcome to The Last Chapter!")
+# add print statement with nicely formatted logo        
         decision_user = input(
             """Do you want to see some books? """
             """Enter Y to see our selection """
@@ -33,13 +34,51 @@ def enter_or_exit():
             print("No worries. Thanks for stopping by!")
             break
 
-
+#good function to use try and except to catch validation error
 def order_book():
     """"
-    Function to order a book, leave & check details
-    or exit
+    Function to select a book
     """
-    pass
+    while True:
+        print(
+            """If you wish to place an order, please """
+            """enter the number of the title you wish """
+            """ to order (i.e. 1 for the first title """
+            """2 for the second title etc.)"""
+        )
+        select_book = input("Enter the number now: ")
+        if select_book == "1":
+            print(f"You selected: {book_list.get('1')}")
+        elif select_book == "2":
+            print(f"You selected: {book_list.get('2')}")
+        elif select_book == "3":
+            print(f"You selected: {book_list.get('3')}")
+        elif select_book == "4":
+            print(f"You selected: {book_list.get('4')}")
+        elif select_book == "5":
+            print(f"You selected: {book_list.get('5')}")
+        elif select_book == "6":
+            print(f"You selected: {book_list.get('6')}")
+        elif select_book == "7":
+            print(f"You selected: {book_list.get('7')}")
+        elif select_book == "8":
+            print(f"You selected: {book_list.get('8')}")
+        else:
+            pass
+
+
+def user_details():
+    while True:
+        print("Great. Let me now grab your details.")
+        enter_fname = input("Please enter your first name: ")
+        enter_lname = input("Please enter your last name: ")
+        email_address = input("Please enter your email address: ")
+
+
+
+
+
+
 
 
 def update_sheet():
