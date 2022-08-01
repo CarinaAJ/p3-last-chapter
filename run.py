15abc462cdf5f07a32d7c312a83c20bdb8b2d873
CarinaAJ <carinastenger@yahoo.com>
2022-08-01 09:15:10 +0000

    Add main functions with pass statement to set structure

--- run.py
+++ run.py
@@ -1,1 +1,51 @@
 import time 
+
+book_list = {
+    "1.": "Title1 - Author1",
+    "2.": "Title2 - Author2",
+    "3.": "Title3 - Author3",
+    "4.": "Title4 - Author4",
+    "5.": "Title5 - Author5",
+    "6.": "Title6 - Author6",
+    "7.": "Title7 - Author7",
+    "8.": "Title8 - Author8",
+}
+
+
+def enter_or_exit():
+    """"
+    Function to either continue to browse
+    or exit
+    """
+    pass
+
+
+def order_book():
+    """"
+    Function to order a book, leave & check details
+    or exit
+    """
+    pass
+
+
+def update_sheet():
+    """"
+    Function to update the Google Sheet
+    with the user's details
+    """
+    pass
+
+
+def print_receipt():
+    """"
+    Function to print the receipt
+    """
+    pass
+
+
+def main():
+    """"
+    Main function, which includes
+    all functions to run the program
+    """
+    pass
