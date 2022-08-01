78fb74ea689b800a556cde9b74c6225ac65c3c30
CarinaAJ <carinastenger@yahoo.com>
2022-08-01 09:49:40 +0000

    Add basic structure enter or exit function and test it

--- run.py
+++ run.py
@@ -11,13 +11,27 @@ book_list = {
     "8.": "Title8 - Author8",
 }
 
-
+#works, just need to make sure to add validation error 
 def enter_or_exit():
     """"
     Function to either continue to browse
     or exit
     """
-    pass
+    while True:
+        print("Hello and welcome to The Last Chapter!")
+        decision_user = input(
+            """Do you want to see some books? """
+            """Enter Y to see our selection """
+            """or N to exit: """
+        )
+        if decision_user == "Y":
+            print("Amazing! This is our list: \n")
+            for key in book_list:
+                print(key, book_list[key])
+            break
+        else:
+            print("No worries. Thanks for stopping by!")
+            break
 
 
 def order_book():
